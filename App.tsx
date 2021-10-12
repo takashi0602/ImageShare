63946afb104aada972a3740113f62e4453e53115
takashi0602 <t.takashi0602@gmail.com>
2021-10-12 22:34:37 +0900

    feat: adding an image

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+// import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#888", fontSize: 18 }}>
+      <Image
+        source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+        style={styles.logo}
+      />
+      {/* <Image source={logo} style={{ width: 305, height: 159 }} /> */}
+      <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button
         below!
       </Text>
@@ -20,5 +26,15 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
   },
 });
